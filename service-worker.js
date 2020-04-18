e78b7c23e12a1978dbe73a74693384e7ad6d46cb
sophie-tsai <55775756+sophie-tsai@users.noreply.github.com>
2020-04-18 16:24:13 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.f2552a6835e9e1b14a101fdc79e5e86f.js"
+  "/Portfolio/precache-manifest.16d08f9ef60067473e8a6f34502ffb5d.js"
 );
 
 self.addEventListener('message', (event) => {
