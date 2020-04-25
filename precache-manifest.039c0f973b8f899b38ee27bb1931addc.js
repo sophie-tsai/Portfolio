bcb2f224bbb941c3e028eab94c7c568842fad807
sophie-tsai <55775756+sophie-tsai@users.noreply.github.com>
2020-04-24 18:04:54 -0700

    Updates

---
+++ precache-manifest.039c0f973b8f899b38ee27bb1931addc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a58365894567a3ea775cb2365b48b600",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e096e35bdc177ac784e9",
+    "url": "/static/css/main.82c86bbd.chunk.css"
+  },
+  {
+    "revision": "a0a4df37f51e6455ab14",
+    "url": "/static/js/2.88ae820c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.88ae820c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e096e35bdc177ac784e9",
+    "url": "/static/js/main.ee52545a.chunk.js"
+  },
+  {
+    "revision": "4c15ceb5499f6abee85b",
+    "url": "/static/js/runtime-main.eb07c764.js"
+  },
+  {
+    "revision": "0522866fdf81b0aa71ad3f2ad4f79586",
+    "url": "/static/media/me-waffles.0522866f.JPG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.c6608e925848d46d8d9f2e8ccd50ee6c.js"
+  "/precache-manifest.039c0f973b8f899b38ee27bb1931addc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
