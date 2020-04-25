df18720aa375803e0268430fdb96a6b5e6de7ef6
sophie-tsai <55775756+sophie-tsai@users.noreply.github.com>
2020-04-25 09:10:28 -0700

    Fixed Hover Glitch Bug

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -9,12 +9,12 @@ function Projects(props) {
 
   /* animation */
   const calc = (x, y) => [
-    -(y - window.innerHeight / 2) / 30,
-    (x - window.innerWidth / 2) / 30,
-    1.001,
+    -(y - window.innerHeight / 2) / 75,
+    (x - window.innerWidth / 2) / 75,
+    1.01,
   ];
   const trans = (x, y, s) =>
-    `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
+    `perspective(800px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
   const [prop, set] = useSpring(() => ({
     xys: [0, 0, 1],
     config: { mass: 1, tension: 120, friction: 14 },
@@ -32,13 +32,10 @@ function Projects(props) {
         onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
         onMouseLeave={() => set({ xys: [0, 0, 1] })}
         style={{ transform: prop.xys.interpolate(trans) }}
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
       >
-        <a
-          className="project-img-container"
-          href={url}
-          onMouseEnter={() => setHovered(true)}
-          onMouseLeave={() => setHovered(false)}
-        >
+        <a className="project-img-container" href={url}>
           <img
             className={`project-image ${hovered && "project-image-hover"}`}
             src={img}
