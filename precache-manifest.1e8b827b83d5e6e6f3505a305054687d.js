735ff2254e3400e076e86fc929d8446a1eb1bc35
sophie-tsai <55775756+sophie-tsai@users.noreply.github.com>
2020-05-15 10:34:33 -0700

    Updates

---
+++ precache-manifest.1e8b827b83d5e6e6f3505a305054687d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e1e2fc49ab14aeb67b92024fc5c1ca0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d775682b0a3cb7a33486",
+    "url": "/static/css/2.ab8a0683.chunk.css"
+  },
+  {
+    "revision": "6fcbb8b997904c1b5c18",
+    "url": "/static/css/main.5d04bb35.chunk.css"
+  },
+  {
+    "revision": "d775682b0a3cb7a33486",
+    "url": "/static/js/2.5684bd33.chunk.js"
+  },
+  {
+    "revision": "78aa0676509153305bde072ed1e9cb1c",
+    "url": "/static/js/2.5684bd33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fcbb8b997904c1b5c18",
+    "url": "/static/js/main.d4a21f44.chunk.js"
+  },
+  {
+    "revision": "4c15ceb5499f6abee85b",
+    "url": "/static/js/runtime-main.eb07c764.js"
+  },
+  {
+    "revision": "0522866fdf81b0aa71ad3f2ad4f79586",
+    "url": "/static/media/me-waffles.0522866f.JPG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.39eb1433c87817eea188b6fa07a8b369.js"
+  "/precache-manifest.1e8b827b83d5e6e6f3505a305054687d.js"
 );
 
 self.addEventListener('message', (event) => {
